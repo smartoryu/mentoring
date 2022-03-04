3d3a6342320403a132b122cc7988128ee4835e9c
prikenang <prikenang@gmail.com>
2022-03-04 13:45:23 +0700

    fundamental sesi 1

---
+++ fundamentals/number.js
@@ -0,0 +1,69 @@
+/**
+ * //* DATA TYPES - NUMBER
+ */
+
+/**
+ * Operator + = Addition
+ * Operator - = Subtraction
+ * Operator * = Multiplication
+ * Operator / = Division
+
+ * Operator % = Modulus
+ * Operator ** = Exponent
+ * Operator ++ = Increment
+ * Operator -- = Decrement
+
+ * Operator += = Addition Assignment
+ * Operator -= = Subtraction Assignment
+ * Operator *= = Multiplication Assignment
+ * Operator /= = Division Assignment
+ * Operator %= = Modulus Assignment
+ * Operator **= = Exponent Assignment
+ */
+
+// var a = 20;
+// var b = 10;
+
+// console.group("Operator + - * /");
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+// console.groupEnd();
+
+// var a = 2;
+// var b = 3;
+
+// console.group("\nOperator % ** ++ --");
+// console.log(a % b);
+// console.log(a ** b);
+// console.log(a++);
+// console.log(a--);
+// console.groupEnd();
+
+// var a = 2;
+// var b = 4;
+
+// console.group("\nOperator Assignment");
+// console.log(a += b);
+// console.log(a -= b);
+// console.log(a *= b);
+// console.log(a /= b);
+// console.log(a %= b);
+// console.log(a **= b);
+// console.groupEnd();
+
+// console.log(Math.PI);
+// console.log(Math.abs(-4.7));
+// console.log(Math.pow(8, 2));
+// console.log(Math.sqrt(64));
+// console.log(Math.cbrt(8));
+
+// console.log(Math.round(4.7));
+// console.log(Math.round(4.4));
+// console.log(Math.floor(4.7));
+// console.log(Math.ceil(4.4));
+
+console.log(Math.random());
+console.log(Math.max(1, 3, 5));
+console.log(Math.min(1, 3, 5));
---
+++ fundamentals/string.js
@@ -0,0 +1,37 @@
+/**
+ * //* DATA TYPES - STRING
+ */
+var nama = "Hello World";
+
+// console.log(nama);
+// console.log(nama.length); // 11
+// console.log(nama.indexOf('World')); // 6
+// console.log(nama.toUpperCase()); // HELLO WORLD
+// console.log(nama.toLowerCase()); // hello world
+// console.log(nama.substring(3, 7)); // lo W
+// console.log(nama.slice(2, 4)); // lo W
+// console.log(nama.split('o')); // ["Hello", "World"]
+// console.log(nama.replace('World', 'Universe')); // Hello Universe
+
+/**
+ * //* DATA TYPES - CONVERT STRING TO NUMBER
+ */
+// let age = 22;
+// let fullname = "John Doe";
+
+// console.log(age + age);
+// console.log(fullname + ' ' + fullname);
+// console.log(fullname + age);
+
+// let stringAge = "22.342";
+
+// console.log(parseFloat(stringAge));
+// console.log(typeof(parseFloat(stringAge)));
+
+// console.log(parseInt(stringAge));
+// console.log(typeof(parseInt(stringAge)));
+
+// console.log(parseFloat(fullname));
+// console.log(parseInt(fullname));
+// console.log(typeof(parseFloat(fullname)));
+// console.log(typeof(parseInt(fullname)));
